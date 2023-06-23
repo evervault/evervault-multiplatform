f58f419138b2d9fd49c1a62be1882f66a0f1ab56
Lammert Westerhoff <westerhoff@gmail.com>
2023-06-23 09:45:57 -0600

    Revert "Remove support for Apple platforms"
    This reverts commit be26e0a5521cb98fdb487d8675f949f99df16e70.

--- evervault-core/build.gradle.kts
+++ evervault-core/build.gradle.kts
@@ -23,6 +23,36 @@ kotlin {
         }
     }
 
+    val applePlatforms: List<KotlinNativeTarget> = listOf(
+
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+        // swiftklib currently doesn't support below platforms in the latest release but support has been added in a recent pull request so next release should support it
+//        macosX64(),
+//        macosArm64(),
+//        watchosX64(),
+//        watchosArm32(),
+//        watchosArm64(),
+//        watchosSimulatorArm64(),
+//        tvosX64(),
+//        tvosArm64(),
+//        tvosSimulatorArm64()
+    )
+
+    applePlatforms.forEach {
+        it.compilations {
+            val main by getting {
+                cinterops {
+                    create("KCrypto")
+                }
+            }
+        }
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -50,6 +80,36 @@ kotlin {
                 implementation("org.bouncycastle:bcprov-jdk15on:1.70")
             }
         }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+            }
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
+    }
+}
+
+swiftklib {
+    create("KCrypto") {
+        path = file("native/KCrypto")
+        packageName("com.evervault.sdk.objclibs.kcrypto")
     }
 }
 
