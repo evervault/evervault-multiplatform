688b08bfeaa712d0dc5a3fe5d8a91dbb6016e53a
Lammert Westerhoff <westerhoff@gmail.com>
2023-07-18 19:32:10 -0600

    Use KMM conventions to publish library

---
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -0,0 +1,91 @@
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.kotlin.dsl.`maven-publish`
+import org.gradle.kotlin.dsl.signing
+import java.util.*
+
+plugins {
+    `maven-publish`
+    signing
+}
+
+// Stub secrets to let the project sync and build without the publication values set up
+ext["signing.keyId"] = null
+ext["signing.password"] = null
+ext["signing.secretKeyRingFile"] = null
+ext["ossrhUsername"] = null
+ext["ossrhPassword"] = null
+
+// Grabbing secrets from local.properties file or from environment variables, which could be used on CI
+val secretPropsFile = project.rootProject.file("local.properties")
+if (secretPropsFile.exists()) {
+    secretPropsFile.reader().use {
+        Properties().apply {
+            load(it)
+        }
+    }.onEach { (name, value) ->
+        ext[name.toString()] = value
+    }
+} else {
+    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+    ext["signing.secretKeyRingFile"] = System.getenv("SIGNING_SECRET_KEY_RING_FILE")
+    ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+    ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
+publishing {
+    // Configure maven central repository
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+        }
+    }
+
+    // Configure all publications
+    publications.withType<MavenPublication> {
+        // Stub javadoc.jar artifact
+        artifact(javadocJar.get())
+
+        // Provide artifacts information requited by Maven Central
+
+        pom {
+            name.set("Evervault")
+            description.set("A library of shared Evervault functionality for Mobile SDK's")
+            url.set("https://github.com/evervault/evervault-multiplatform")
+            developers {
+                developer {
+                    name.set("engineering")
+                    organization.set("Evervault")
+                    email.set("engineering@evervault.com")
+                }
+            }
+            scm {
+                connection.set("scm:git:ssh://git@github.com:evervault/evervault-multiplatform.git")
+                url.set("https://github.com/evervault/evervault-multiplatform")
+            }
+            licenses {
+                license {
+                    name.set("The MIT License (MIT)")
+                    url.set("https://mit-license.org")
+                }
+            }
+        }
+    }
+}
+
+// Signing artifacts. Signing.* extra properties values will be used
+signing {
+    sign(publishing.publications)
+}
